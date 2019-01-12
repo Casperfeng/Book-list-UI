a942f4726109d9b68836e6b5a6389ff7183f655d
Casperfeng <casp.feng@gmail.com>
2019-01-12 15:56:49 +0100

    now adds books to archive

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-// Book constructor
-function Book(title, author, isbn){
+// Book Constructor
+function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
 }
 
-// UI constructor
-function UI(){
+// UI Constructor
+function UI() {}
 
+// Add Book To List
+UI.prototype.addBookToList = function(book){
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  `;
+
+  list.appendChild(row);
 }
 
-UI.prototype.addBookToList = function(book){
-  
+// Clear Fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
 }
 
 // Event Listeners
-document.getElementById("book-form").addEventListener("submit", function(e){
-
+document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
-  const title = document.getElementById("title").value,
-  author = document.getElementById("author").value,
-  isbn = document.getElementById("isbn").value;
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
 
   // Instantiate book
   const book = new Book(title, author, isbn);
 
   // Instantiate UI
   const ui = new UI();
-
+ 
   // Add book to list
   ui.addBookToList(book);
 
+  // Clear fields
+  ui.clearFields();
+
   e.preventDefault();
-})
+});
